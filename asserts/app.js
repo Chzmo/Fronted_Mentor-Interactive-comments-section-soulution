dd254ba93b01ab5395b68269d27ed453e804e979
chzmo <chisomomoyo10@gmail.com>
2022-09-27 17:14:06 +0200

    inserting elements on ui

--- asserts/app.js
+++ asserts/app.js
@@ -1,3 +1,105 @@
+// Create HTML Elements
+class createElements{
+
+    static createComment(comment){
+        return `
+            <div class="container__content-left br-2">
+                <div class="container__content-left_plus">
+                <a href=""><img src="./images/icon-plus.svg" alt=""></a>
+                </div>
+                <div class="container__content-left_score">
+                <P>${comment.score}</P>
+                </div>
+                <div class="container__content-left_minus">
+                <a href=""><img src="./images/icon-minus.svg" alt=""></a>
+                </div>
+            </div>
+
+            <div class="container__content-right">
+                <div class="container__content-right_top">
+                <div class="container__content-right_top-profile">
+                    <div class="container__content-right_top-profile_img">
+                    <img src="${comment.user.image.png}" alt="amyrobson">
+                    </div>
+                    <div class="container__content-right_top-profile_username"><p>${comment.user.username}</p></div>
+                    <div class="container__content-right_top-profile_time"><p>${comment.createdAt}</p></div>
+                </div>
+                <div class="container__content-right_top-reply">
+                    <a href="" class="comment_reply" id="${comment.id}"><span><img src="./images/icon-reply.svg" alt="reply-icon" ></span> Reply</a>
+                </div>
+                </div>
+                <div class="container__content-right_bottom">
+                <p>${comment.content}</p>
+                </div>
+            </div>
+        `;
+    }
+
+    static createReply(reply, data){
+        let user_data = '';
+
+        user_data +=`                    
+            <div class="container__content br-1 ">
+                <div class="container__content-left br-2">
+                    <div class="container__content-left_plus">
+                        <a href=""><img src="./images/icon-plus.svg" alt=""></a>
+                    </div>
+                    <div class="container__content-left_score">
+                        <P>${reply.score}</P>
+                    </div>
+                    <div class="container__content-left_minus">
+                        <a href=""><img src="./images/icon-minus.svg" alt=""></a>
+                    </div>
+                    </div>
+            
+                    <div class="container__content-right">
+                    <div class="container__content-right_top">
+                        <div class="container__content-right_top-profile">
+                        <div class="container__content-right_top-profile_img">
+                            <img src=".${reply.user.image.png}" alt="amyrobson">
+                        </div>
+                        <div class="container__content-right_top-profile_username"><p>${reply.user.username}</p></div>
+                        <div class="container__content-right_top-profile_time"><p>${reply.createdAt}</p></div>
+                        </div>
+                        <div class="container__content-right_top-reply">
+                        `;
+                        
+                        if (data.currentUser.username === reply.user.username){
+                            user_data +=    `<a href="" class="red"><span><img src="./images/icon-delete.svg" /></span> Delete</a> 
+                                            &emsp;
+                                            <a href=""><img src="./images/icon-edit.svg" /> <span>Edit</span></a>`;
+                        } else{
+                            user_data += `<a href=""><img src="./images/icon-reply.svg" alt="reply-icon" > <span>Reply</span></a>`;
+                        }
+
+                        user_data += `
+                        </div>
+                    </div>
+                    <div class="container__content-right_bottom">
+                        <p><a>@${reply.replyingTo}</a> ${reply.content}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        return user_data;
+    }
+
+    static CreateInputField(image){
+        return `
+        <form class="container__form br-1" id="form_comment">
+            <div class="container__form-img">
+                <img src="${image}" alt="">
+            </div>
+            <div class="container__form-input">
+                <input class="br-1" type="textarea" name="reply" id="reply_comment" placeholder="Add a Comment" required>
+            </div>
+            <div class="container__reply-button">
+                <input type="submit" class="btn " id="btn_submit" value="${'SEND'}">
+            </div>
+        </form> `;
+    }
+}
 
 //Store Class: Handles Storage
 class Store {
@@ -42,111 +144,25 @@ class Store {
 
 // UI Class: Handle UI Tasks
 class UI {
-  
+
     static displayComments() {
         let data = Store.getDataComments();
-        let user_data = ''
-        let container = document.querySelector('.container');
+        let user_data = '';
         for(let comment of data.comments){
-            user_data += `
-            <div class="container__content br-1">
-                <div class="container__content-left br-2">
-                    <div class="container__content-left_plus">
-                    <a href=""><img src="./images/icon-plus.svg" alt=""></a>
-                    </div>
-                    <div class="container__content-left_score">
-                    <P>${comment.score}</P>
-                    </div>
-                    <div class="container__content-left_minus">
-                    <a href=""><img src="./images/icon-minus.svg" alt=""></a>
-                    </div>
-                </div>
-
-                <div class="container__content-right">
-                    <div class="container__content-right_top">
-                    <div class="container__content-right_top-profile">
-                        <div class="container__content-right_top-profile_img">
-                        <img src="${comment.user.image.png}" alt="amyrobson">
-                        </div>
-                        <div class="container__content-right_top-profile_username"><p>${comment.user.username}</p></div>
-                        <div class="container__content-right_top-profile_time"><p>${comment.createdAt}</p></div>
-                    </div>
-                    <div class="container__content-right_top-reply">
-                        <a href=""><span><img src="./images/icon-reply.svg" alt="reply-icon" ></span> Reply</a>
-                    </div>
-                    </div>
-                    <div class="container__content-right_bottom">
-                    <p>${comment.content}</p>
-                    </div>
-                </div>
-            </div>
-            `;
+            user_data += `<div class="container__content br-1">`;
+            user_data += createElements.createComment(comment);
+            user_data += `</div>`;
 
             if (comment.replies !== null){
                 user_data += `<div class="comment">`;
                 for(let reply of comment.replies){
-                    user_data +=`                    
-                        <div class="container__content br-1 ">
-                            <div class="container__content-left br-2">
-                            <div class="container__content-left_plus">
-                                <a href=""><img src="./images/icon-plus.svg" alt=""></a>
-                            </div>
-                            <div class="container__content-left_score">
-                                <P>${reply.score}</P>
-                            </div>
-                            <div class="container__content-left_minus">
-                                <a href=""><img src="./images/icon-minus.svg" alt=""></a>
-                            </div>
-                            </div>
-                    
-                            <div class="container__content-right">
-                            <div class="container__content-right_top">
-                                <div class="container__content-right_top-profile">
-                                <div class="container__content-right_top-profile_img">
-                                    <img src=".${reply.user.image.png}" alt="amyrobson">
-                                </div>
-                                <div class="container__content-right_top-profile_username"><p>${reply.user.username}</p></div>
-                                <div class="container__content-right_top-profile_time"><p>${reply.createdAt}</p></div>
-                                </div>
-                                <div class="container__content-right_top-reply">
-                                `;
-                                
-                                if (data.currentUser.username === reply.user.username){
-                                    user_data +=    `<a href="" class="red"><span><img src="./images/icon-delete.svg" /></span> Delete</a> 
-                                                    &emsp;
-                                                    <a href=""><img src="./images/icon-edit.svg" /> <span>Edit</span></a>`;
-                                } else{
-                                    user_data += `<a href=""><img src="./images/icon-reply.svg" alt="reply-icon" > <span>Reply</span></a>`;
-                                }
-
-                                user_data += `
-                                </div>
-                            </div>
-                            <div class="container__content-right_bottom">
-                                <p>${reply.content}</p>
-                            </div>
-                            </div>
-                        </div>
-                    `;
+                    user_data += createElements.createReply(reply, data);
                 }
                 user_data += `</div>`;
             }
         }
 
-        user_data += `
-        
-        <form class="container__form br-1" id="form_comment">
-            <div class="container__form-img">
-                <img src="${data.currentUser.image.png}" alt="">
-            </div>
-            <div class="container__form-input">
-                <input class="br-1" type="textarea" name="reply" id="reply_comment" placeholder="Add a Comment" required>
-            </div>
-            <div class="container__reply-button">
-                <input type="submit" class="btn " id="btn_submit" value="${'SEND'}">
-            </div>
-        </form>        
-        `
+        user_data += createElements.CreateInputField(data.currentUser.image.png);
         document.querySelector('.container').innerHTML = user_data;
     }
 
@@ -156,37 +172,7 @@ class UI {
         let form = document.querySelector('#form_comment');
         let div = document.createElement('div');
         div.classList = "container__content br-1";
-        let child = `
-        <div class="container__content-left br-2">
-            <div class="container__content-left_plus">
-            <a href=""><img src="./images/icon-plus.svg" alt=""></a>
-            </div>
-            <div class="container__content-left_score">
-            <P>${comment.score}</P>
-            </div>
-            <div class="container__content-left_minus">
-            <a href=""><img src="./images/icon-minus.svg" alt=""></a>
-            </div>
-        </div>
-
-        <div class="container__content-right">
-            <div class="container__content-right_top">
-            <div class="container__content-right_top-profile">
-                <div class="container__content-right_top-profile_img">
-                <img src="${comment.user.image.png}" alt="amyrobson">
-                </div>
-                <div class="container__content-right_top-profile_username"><p>${comment.user.username}</p></div>
-                <div class="container__content-right_top-profile_time"><p>${comment.createdAt}</p></div>
-            </div>
-            <div class="container__content-right_top-reply">
-                <a href=""><span><img src="./images/icon-reply.svg" alt="reply-icon" ></span> Reply</a>
-            </div>
-            </div>
-            <div class="container__content-right_bottom">
-            <p>${comment.content}</p>
-            </div>
-        </div>
-        `;
+        let child = createElements.createComment(comment);
 
         div.innerHTML = child;
         container.insertBefore(div, form);
@@ -196,18 +182,6 @@ class UI {
     //   if(el.classList.contains('delete')) {
     //     el.parentElement.parentElement.remove();
     //   }
-    // }
-  
-    // static showAlert(message, className) {
-    //   const div = document.createElement('div');
-    //   div.className = `alert alert-${className}`;
-    //   div.appendChild(document.createTextNode(message));
-    //   const container = document.querySelector('.container');
-    //   const form = document.querySelector('#book-form');
-    //   container.insertBefore(div, form);
-  
-    //   // Vanish in 3 seconds
-    //   setTimeout(() => document.querySelector('.alert').remove(), 3000);
     // }
   
     static clearFields() {
@@ -243,7 +217,7 @@ http.onload = function(){
             let comment = document.querySelector('#reply_comment').value;
 
             if (comment ===''){
-                 alert('Please fill all fields');
+                alert('Please fill all fields');
             } else{
                 userComment = {
                     "id": Math.floor((Math.random() + Math.random()) * 3456),
@@ -269,21 +243,8 @@ http.onload = function(){
                 }else{
                     alert('something happened');
                 };
-
-            //     const book = new Book(title, author, isbn);
-        
-            //     //Add book to UI
-            //     UI.addBookToList(book);
-        
-            //     //add books to storage
-            //     Store.addBook(book);
-        
-            //     UI.showAlert('Book Added Successfully', 'success');
-        
-            //     //clear fields
-            //     UI.clearFiels();
+            
             }
-        
         });
     }
 }
